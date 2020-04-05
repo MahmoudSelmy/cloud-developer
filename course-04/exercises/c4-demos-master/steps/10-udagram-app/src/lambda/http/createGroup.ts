cc9f80e032111395dfa0a6c413767965f8d30486
mahmoudselmy06@gmail.com <mahmoud_sam_06>
2020-04-05 17:30:58 +0200

    + create group api + validate group api

--- course-04/exercises/c4-demos-master/steps/10-udagram-app/src/lambda/http/createGroup.ts
+++ course-04/exercises/c4-demos-master/steps/10-udagram-app/src/lambda/http/createGroup.ts
@@ -1,27 +1,34 @@
 import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import 'source-map-support/register'
 import * as AWS  from 'aws-sdk'
-
+import * as uuid from 'uuid'
 
 const docClient = new AWS.DynamoDB.DocumentClient()
 const groupsTable = process.env.GROUPS_TABLE
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   console.log('Processing event: ', event)
+  const itemId = uuid.v4()
 
-  const result = await docClient.scan({
-    TableName: groupsTable
+  const parsedBody = JSON.parse(event.body)
+
+  const newItem = {
+    id: itemId,
+    ...parsedBody
+  }
+
+  await docClient.put({
+    TableName: groupsTable,
+    Item: newItem
   }).promise()
 
-  const items = result.Items
-
   return {
-    statusCode: 200,
+    statusCode: 201,
     headers: {
       'Access-Control-Allow-Origin': '*'
     },
     body: JSON.stringify({
-      items
+      newItem
     })
   }
 }
