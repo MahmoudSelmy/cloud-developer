71be3f5e73e81f9fc25e6fdfa910114946b55318
mahmoudselmy06@gmail.com <mahmoud_sam_06>
2020-04-29 03:16:12 +0200

    + delete todo

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/deleteTodo.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/deleteTodo.ts
@@ -2,9 +2,20 @@ import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 
+import {getUserId} from '../utils'
+import { deleteTodo } from '../../buisnessLogic/todo'
+
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
 
   // TODO: Remove a TODO item by id
-  return undefined
+  const userId = getUserId(event)
+
+  await deleteTodo(todoId, userId)
+
+  return {
+    statusCode: 200,
+    headers: {'Access-Control-Allow-Origin': '*'},
+    body: JSON.stringify({})
+  }
 }
