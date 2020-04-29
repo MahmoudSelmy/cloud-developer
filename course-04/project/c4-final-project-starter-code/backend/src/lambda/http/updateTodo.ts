2458dc4fd3dd6177ffdfa500ada858749f8b49f6
mahmoudselmy06@gmail.com <mahmoud_sam_06>
2020-04-29 03:13:35 +0200

    + update todo

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/updateTodo.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/updateTodo.ts
@@ -3,11 +3,21 @@ import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
 
 import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+import { updateTodo } from '../../buisnessLogic/todo'
+import {getUserId} from '../utils'
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
-  const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
+  const updatedTodoRequest: UpdateTodoRequest = JSON.parse(event.body)
 
   // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-  return undefined
+  const userId = getUserId(event);
+
+  await updateTodo(todoId, userId, updatedTodoRequest)
+
+  return {
+    statusCode: 200,
+    headers: {'Access-Control-Allow-Origin': '*'},
+    body: JSON.stringify({})
+  }
 }
