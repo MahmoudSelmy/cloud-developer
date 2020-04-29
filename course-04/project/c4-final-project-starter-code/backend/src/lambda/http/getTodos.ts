c3cd23eb8198c3d95c082ff941db9c0c3c6fcaaf
mahmoudselmy06@gmail.com <mahmoud_sam_06>
2020-04-29 02:23:11 +0200

    + get todo

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
@@ -1,7 +1,19 @@
 import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import {getUserId} from '../utils'
+import { getUserTodoItems } from '../../buisnessLogic/todo'
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => 
+{
   // TODO: Get all TODO items for a current user
+  const userId = getUserId(event);
+  const todos = await getUserTodoItems(userId)
+
+  return {
+    statusCode: 200,
+    headers: {'Access-Control-Allow-Origin': '*'},
+    body: JSON.stringify({items: todos})
+  }
+
 }
